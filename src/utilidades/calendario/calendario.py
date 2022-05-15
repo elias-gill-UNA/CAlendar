c418ee09c55301dcb619b6ae767fc790caa8e04c
elias-gill-UNA <100032172+elias-gill-UNA@users.noreply.github.com>
2022-05-15 19:47:14 -0400

    Como de debe de ser
    Este modulo es un modulo intermedio que solo debe retornar el dia y si es feriado o no, agregar todos los feriados, revisar logica, optimizar y comentar mas.

--- src/utilidades/calendario/calendario.py
+++ src/utilidades/calendario/calendario.py
@@ -1,208 +1,35 @@
-"""def calendario (año):
-    for i in range(1,13) :
-        print("mes:",nombre_mes(i))
-        print("Dom Lun Mar Mie Jue Vie Sab ")
-        z=zeller(año,i)
-        print("z:",z)
-        for k in range(0,z) :
-            print("  ",end="")
+#NOTE  Poner los feriados en un objeto, la funcion itera sobre ellos
+feriados = {
+    "1": [1, 1, "Primero de enero"],
+    "2": ["dia", "mes", "fiesta"],
+    "3": ["dia", "mes", "fiesta"],
+    # ..... 
+}
 
-        diasmes=calculardiasMes(año,i)
-
-        for j in range(1, diasmes+1) :
-            if (j%7==0 or j==30 or j==31):
-                print(j)
-            else:
-                print(j,end="   ")
-
-def calculardiasMes(año,mes):
-    if (mes ==1 or mes== 3 or mes== 5 or mes ==7 or mes==8 or mes==10 or mes==12 ):
-        return 31
-    elif mes==2:
-        if (añobisiesto(año)):
-
-            return 29
-        else:
-            return 28
-
-    elif (mes==4 or mes==6 or mes==9 or mes==11):
-        return 30
-"""
 def zeller(año,mes,dia):
-
     a=int((14-mes)/12)
     y=año-a
     m=mes+ 12*a - 2
     d=int((dia+y+int(y/4)-int(y/100)+int(y/400)+int((31*m)/12)) % 7)
-
     return d
 
-def verif_feriado(mes,dia):
 
-    if (dia == 1 and mes == 1):
-        return 1
+# NOTE  Funcio que retorne {"feriado": True/False, "fiesta": "fiesta/fin de semana/trabaja"}
+def feriado(año,mes,dia): 
+    for key in feriados.keys(): # si es feriado
+        if(dia, mes == key[0], key[1]):
+            return {"res": True, "fiesta": "Primero de enero"}
+    return finSemana(año,mes,dia) # si es que no retorna si es fin de semana o no
 
-    elif (dia == 1 and mes == 3):
-        return 1
-
-    elif (dia == 1 and mes == 5):
-        return 1
-
-    elif (dia == 14 and mes == 5):
-        return 1
-
-    elif (dia == 15 and mes == 5):
-        return 1
-
-    elif (dia == 12 and mes == 6):
-        return 1
-
-    elif (dia == 15 and mes == 8):
-        return 1
-
-    elif (dia == 29 and mes == 9):
-        return 1
-
-    elif (dia == 8 and mes == 12):
-        return 1
-
-    elif (dia == 25 and mes == 12):
-        return 1
-    else:
-        return 0
-
-def feriado(año,mes,dia):
-
-
-    dia_correspondiente(zeller(año,mes,dia))
-
-    if (dia==1 and mes==1):
-        print("AÑO NUEVO")
-
-
-    elif (dia == 1 and mes == 3):
-        print("DIA DE LOS HEROES")
-
-
-    elif (dia==1 and mes==5):
-        print("DIA DEL TRABAJADOR")
-
-
-    elif ((dia==14 and mes==5) or (dia==15 and mes==5)):
-        print("DIA DE LA PATRIA")
-
-
-    elif (dia==12 and mes==6):
-        print("PAZ DEL CHACO")
-
-
-    elif (dia==15 and mes==8):
-        print("FUNDACION DE ASUNCION")
-
-
-    elif (dia == 29 and mes == 9):
-        print("BATALLA DE BOQUERON")
-
-
-    elif (dia == 8 and mes == 12):
-        print("DIA DE LA VIRGEN DE CAACUPE")
-
-
-    elif (dia == 25 and mes == 12):
-        print("NAVIDAD")
-
-def verif_fin_semana(año,mes,fecha):
-
+# muchas funciones al pedo
+def finSemana(año,mes,fecha):
     d = zeller(año, mes, fecha)
     if d == 6 or d==0:
-        return 1
+        return True
     else:
-        return 0
+        return False
 
 
+# NOTE  ESTE MODULO SOLO DEBE RETORNAR SI ES DIA LABORAL O NO
 
-def fines_semana(año,mes,fecha):
-
-    d=zeller(año,mes,fecha)
-
-    if d==6:
-        print("ES SABADO NO SE TRABAJA")
-    else:
-        print("ES DOMINGO NO SE TRABAJA")
-
-def dia_correspondiente(x):
-    if(x==0):
-        print("ES DOMINGO")
-    elif x==1:
-        print("ES LUNES")
-    elif x==2:
-        print("ES MARTES")
-    elif x==3:
-        print("ES MIERCOLES")
-    elif x == 4:
-        print("ES JUEVES")
-    elif x == 5:
-        print("ES VIERNES")
-    elif x == 6:
-        print("ES SABADO")
-
-"""
-def añobisiesto(año):
-    if(año % 4 ==0 and año%100!=0 or año%400==0 ):
-        return 1
-    else:
-        return 0
-
-def nombre_mes(mes):
-
-    if mes == 1:
-        return "enero"
-    elif mes == 2:
-        return "Febrero"
-    elif mes == 3:
-        return "Marzo"
-    elif mes == 4:
-        return "Abril"
-    elif mes == 5:
-        return "Mayo"
-    elif mes == 6:
-        return "Junio"
-
-    elif mes == 7:
-        return "Julio"
-
-    elif mes == 8:
-        return "Agosto"
-    elif  mes == 9:
-        return "Septiembre"
-    elif mes == 10:
-        return "Octubre"
-
-    elif mes == 11:
-        return "Noviembre"
-    else:
-        return "Diciembre"
-
-mes=5
-
-año=int(input("ingrese año:"))
-calendario(año)
-"""
-##print("hola", end=" ")
-##print("que tal")
-
-año=int(input("ingrese el año:"))
-mes=int(input("ingrese el mes:"))
-fecha=int(input("ingrese la fecha:"))
-###fines_semana(año,mes,fecha)
-###feriado(fecha,mes)
-
-if(verif_feriado(mes,fecha)):
-    feriado(año,mes,fecha)
-
-elif(verif_fin_semana(año,mes,fecha)):
-    fines_semana(año,mes,fecha)
-else:
-    dia_correspondiente(zeller(año,mes,fecha))
-    print("SE TRABAJA")
 
