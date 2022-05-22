6f05916e0b0a87fd19e9b67919c9d231b924a07b
Elias Gill <eliasgill42@gmail.com>
2022-05-22 00:04:12 -0400

    terminado modulo proyectManager

---
+++ src/backend/dataBase/activitiesManager.py
@@ -0,0 +1,15 @@
+def anadirActividad():
+    pass
+
+def eliminarActividad():
+    pass
+
+def modificarActividad():
+    pass
+
+def ():
+    pass
+def anadirActividad():
+    pass
+def anadirActividad():
+    pass
--- src/backend/dataBase/dbFunctions/db_proyects.py
+++ src/backend/dataBase/dbFunctions/db_proyects.py
@@ -1,17 +1,17 @@
 import sqlite3
 
 def nuevoProyecto(proyecto, id): # nombre del proyecto
-    conn = sqlite3.connect(f'../proyects/{id}.db') # crea la base de datos si no existe
+    conn = sqlite3.connect(f'../proyects/{id}.db') # crea la base de datos 
     cursor = conn.cursor()
 
     # Crear las tablas necesarias para el proyecto
-    cursor.execute("CREATE TABLE IF NOT EXISTS Descripcion (id INT, name TEXT, desc TEXT, fecha TEXT, actiCount INT, depCount INT)") #  tabla de descripcion del proyecto
+    cursor.execute("CREATE TABLE IF NOT EXISTS Descripcion (name TEXT, desc TEXT, fechaInicio TEXT, actiCount INT, depCount INT)") #  tabla de descripcion del proyecto
     cursor.execute("CREATE TABLE IF NOT EXISTS Actividades (id INT, name TEXT, duracion INT, fechaTemp TEXT, fechaTar Text, finalizado INT)") # tabla de actividades
-    cursor.execute("CREATE TABLE IF NOT EXISTS Dependecias (id INT, idAntes TEXT, idDespues TEXT)") # tabla de Dependecias
+    cursor.execute("CREATE TABLE IF NOT EXISTS Dependencias (id INT, idAntes TEXT, idDespues TEXT)") # tabla de Dependecias
 
     # Llenado de los campos de identificacion del proyecto
-    cursor.execute("INSERT INTO Descripcion (id, name, desc, fecha, actiCount, depCount) VALUES (?, ?, ?, ?, ?, ?)", 
-              (proyecto.identificador, proyecto.nombre, proyecto.descripcion, proyecto.inicioPrevisto, 0, 0))
+    cursor.execute("INSERT INTO Descripcion (name, desc, fechaInicio, actiCount, depCount) VALUES (?, ?, ?, ?, ?)", 
+              (proyecto.nombre, proyecto.descripcion, proyecto.inicioPrevisto, 0, 0))
 
     # Proyecto creado satisfactoriamente
     conn.commit()
@@ -19,32 +19,37 @@ def nuevoProyecto(proyecto, id): # nombre del proyecto
     return True 
 
 # retorna la tabla de informacion del proyecto
-def getInfo(id):
-    conn = sqlite3.connect(f'../proyects/{id}.db')
+def getInfo(id, conexion):
+    if(conexion): # si se tiene una conexion establecida
+        conexion.execute('SELECT * FROM Descripcion')
+        data = conexion.fetchall()
+        return data
+        
+    conn = sqlite3.connect(f'../proyects/{id}.db') # si es que no 
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM Descripcion')
     data = cursor.fetchall()
     cursor.close()
     return data
-    
+
+def modificarValor(conexion, tabla, parametro, valor, condicion):
+    conexion.execute(f"UPDATE {tabla} SET {parametro} = {valor} {condicion}") # cambiar el valor en alguna tabla
+    conexion.commit()
+    return True
 
 def abrirProyecto(id):
-    pass
+    conn = sqlite3.connect(f'../proyects/{id}.db') # abre el proyecto
+    cursor = conn.cursor()
+    return cursor # retorna la conexion al proyecto
+
+def cerrarProyecto(cursor):
+    cursor.close()
+    return True
 
 
-
-# def dynamic_data_entry(i):
-#     keyword = f'Numero {i}'
-#     value = random.randrange(0,10)
-
-#     c.execute("INSERT INTO tabla1 (palabraclave, valor) VALUES (?, ?)",
-#               (keyword, value))
+#     c.execute("INSERT INTO tabla1 (palabraclave, valor) VALUES (?, ?)", (keyword, value))
 #     conn.commit()
 
-# def read_from_db():
 #     c.execute('SELECT * FROM tabla1')
 #     data = c.fetchall()
-#     print(data)
-#     for row in data:
-#         print(row)
 
