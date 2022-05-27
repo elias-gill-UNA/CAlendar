ee9685ac2a5a87e44f10a657003158b5ee7b3878
Elias Gill <eliasgill42@gmail.com>
2022-05-27 19:05:10 -0400

    la mierda de los imports

--- src/setup.py
+++ src/setup.py
@@ -10,7 +10,11 @@ setup(
         'GUI', 
         'informes', 
         'utilidades', 
+<<<<<<< HEAD
         'backend', 'backend.dataBase', 'backend.comprobaciones', 'backend.dataBase.dbFunctions'
+=======
+        'backend', 'backend.dataBase', 'backend.comprobaciones' 
+>>>>>>> 9182951 (la mierda de los imports)
     ]
     # scripts=[]
 )
