09b743b80a1de63d07c8940f0226a7bdd0561b5b
androceo123 <105460729+androceo123@users.noreply.github.com>
2022-05-27 11:41:16 -0400

    Update camino_critico.py

--- src/informes/camino_critico.py
+++ src/informes/camino_critico.py
@@ -109,5 +109,11 @@ def __inicializar(listaActividades):
 
     for actividad in listaActividades:
         actividad.siguientesPendientes = len(actividad.siguientes)
+    for actividad in listaActividades:#para hacer que las actividades sin antecedentes tengan como antecedentes a la actividad imaginaria "inicio"
+        if len(actividad.anteriores) == 0 and actividad.nombre!="Inicio" and actividad.nombre!="Fin":
+            actividad.anteriores.append(listaActividades[0])
+            listaActividades[0].siguientes.append(actividad)
 
+        if len(actividad.siguientes) == 0 and actividad.nombre!="Inicio" and actividad.nombre!="Fin":#para hacer que las actividades sin descendientes sean antecedentes a la actividad imaginaria "fin"
+            listaActividades[len(listaActividades)-1].anteriores.append(actividad)
 
