26ec1b329fcc8d5092a8422814cb08c7eb0f6c05
AFFG69 <afgabazza@fpuna.edu.py>
2022-05-27 10:43:54 -0400

    saco unas lineas que estaban demas

--- src/utilidades/calendario/usuarios.py
+++ src/utilidades/calendario/usuarios.py
@@ -4,8 +4,6 @@ import cv2 #de libreria opencv-python
 class login():
 
     def __init__(self):
-        self.msg = ""
-        self.cont = 0
         self.window()
         self.titulos()
         self.entrada_datos()
@@ -19,21 +17,18 @@ class login():
         self.ventana.configure(bg="silver")
         self.ventana.title("Identificacion")
         self.ventana.resizable(False,False)
-
         self.ventana.geometry("560x330")
 
 
     def titulos(self):
         t1=Label(self.ventana,fg="black",font=("Arial",18),bg="silver",text="Nombre").place(x=230,y=30)
         t2=Label(self.ventana,bg="silver",fg="black",font=("Arial",18),text="Apellido").place(x=230,y=120)
-        self.t3 = Label(self.ventana,fg="black",font=("Arial",14),bg="silver",text =self.msg)
-        self.t3.place(x=200,y=220)
+
 
     def entrada_datos(self):
         self.username=StringVar()
         self.last_name=StringVar()
         self.nombre = Entry(self.ventana,font=("Arial",18),fg="blue",textvariable=self.username).place(x=150, y=80)
-
         self.apellido = Entry(self.ventana, font=("Arial", 18), fg="blue",textvariable=self.last_name)
         self.apellido.place(x=150, y=180)
 
@@ -42,13 +37,13 @@ class login():
 
         self.aceptar = Button(self.ventana, relief="raised", text="Aceptar", bg="blue", fg="white", font=("Arial", 13),
                               cursor="hand2", command=self.ventana.destroy).place(x=150, y=260, width=110, height=30)
-        self.cancelar = Button(self.ventana, relief="raised", text="Cancelar", bg="blue", fg="white",
-                               font=("Arial", 13),
+        self.cancelar = Button(self.ventana, relief="raised", text="Cancelar", bg="blue", fg="white",font=("Arial", 13),
                                cursor="hand2", command=self.ventana.destroy).place(x=310, y=260, width=110, height=30)
+
+
 
     def creacion_icono(self):
         self.username_data = self.username.get()
-
         self.last_name_data = self.last_name.get()
 
         img = cv2.imread('imagen.jpg')
@@ -63,5 +58,6 @@ class login():
             extension = self.last_name_data + ".jpg"
             cv2.putText(img_resized, texto, posicion, font, tamanoletra, colorletra, grosorletra)
             cv2.imwrite(extension, img_resized)
+
 
 a=login()
