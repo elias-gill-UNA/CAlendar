cc27b28e240f9e7a09f30d079a70e57ffc9eaa7e
Elias Gill <eliasgill42@gmail.com>
2022-05-29 13:35:27 -0400

    instrucciones nuevas para solucionar los imports

---
+++ src/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(
+    name="CAlendar",
+    version="0.1",
+    description="Proyecto para intentar pasar de curso",
+    author="Grupo 11 algoritmos",
+    url="http://github.com/elias-gill-UNA/CAlendar",
+    packages=[
+        'clases', 
+        'GUI', 
+        'informes', 
+        'utilidades', 
+        'backend', 'backend.dataBase', 'backend.comprobaciones', 'backend.dataBase.dbFunctions'
+    ]
+    # scripts=[]
+)
