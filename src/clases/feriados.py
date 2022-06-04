22b811a41df66f5c3229fd56759842271f70e916
Elias Sebastian Gill Quintana <79729891+Elias-Gill@users.noreply.github.com>
2022-06-03 22:37:59 -0400

    Minimo cambio en sintaxis

--- src/clases/feriados.py
+++ src/clases/feriados.py
@@ -1,6 +1,6 @@
 class Feriado:
     def __init__(self, descripcion, dia, mes):
         self.identificador = 0
-        self.feriado = descripcion
+        self.descripcion = descripcion
         self.dia = dia
         self.mes = mes
